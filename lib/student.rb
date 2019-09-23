5ea1895ab33aeb4def526f12975468e816358871
Emirhan Kaplan <emskaplann@gmail.com>
2019-09-23 10:47:02 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require './lib/user.rb'
+require 'pry'
+class Student < User
+    attr_accessor :first_name, :last_name
+    def initialize()
+        @knowledge = []
+    end
 
+    def learn(str)
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    
 end
